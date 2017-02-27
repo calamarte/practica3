065f380269c6e5be47d9ec3799db1c06aed8110e
calamarte <calamarte@gmail.com>
2017-02-27 15:07:55 +0100

    Yo veo un bidimensional en plan transposition

--- src/Printer.java
+++ src/Printer.java
@@ -10,17 +10,19 @@ public class Printer {
     Printer(String alphabet) {
         int longitud;
         int filas = 0;
-        int fila = 0;
+        int columnas = 0;
         int posicion = 0;
 
+        //cuenta las columnas excluyendo '\n'
         for (int i = 0; i < alphabet.length() ; i++) {
             if (alphabet.charAt(i) == '\n') {
-                fila = i-1;// ojo
+                columnas = i-1;// ojo
                 break;
             }
 
         }
 
+        //cuenta las filas
         for (int i = 0; i < alphabet.length() ; i++) {
             if (alphabet.charAt(i) == '\n') {
                 filas++;
@@ -28,36 +30,37 @@ public class Printer {
         }
         filas++;
 
-        longitud = (fila - 25) / 27;
+        //cuantas columnas por letra
+        longitud = (columnas - 25) / 27;
 
-        System.out.println(fila);
+        System.out.println(columnas);
         System.out.println(filas);
         System.out.println(longitud);
 
-        String [] abc = new String[27];
+        String [][] abc = new String[filas][27];
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < filas ; i++) {
-            for (int j = 0,x = 0; j < fila ; j++) {
-                if (x == longitud){
-                    if (i != filas-1)sb.append('\n');
-                    abc[posicion] += sb.toString();
-                    sb.setLength(0);
-                    posicion++;
-                    x = 0;
-                    continue;
-                }
+//        for (int i = 0; i < filas ; i++) {
+//            for (int j = 0,x = 0; j < columnas ; j++) {
+//                if (x == longitud){
+//                    if (i != filas-1)sb.append('\n');
+//                    abc[posicion][] += sb.toString();
+//                    sb.setLength(0);
+//                    posicion++;
+//                    x = 0;
+//                    continue;
+//                }
+//
+//                if (x < longitud+3){
+//                    sb.append(alphabet.charAt(i));
+//                    x++;
+//                }
+//            }
+//            posicion = 0;
+//
+//        }
 
-                if (x < longitud+3){
-                    sb.append(alphabet.charAt(i));
-                    x++;
-                }
-            }
-            posicion = 0;
-
-        }
-
-        System.out.println(Arrays.toString(abc));
+        System.out.println(Arrays.deepToString(abc));
 
 
     }
